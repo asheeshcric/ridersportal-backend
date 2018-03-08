56d9f2524740309d69893fa39571ee92a1a4d23e
asheeshcric <ashiz2013@gmail.com>
2018-03-09 00:16:36 +0545

    [CHANGE] The response format of the register_view

--- ridersportal_app/views/register_view.py
+++ ridersportal_app/views/register_view.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.hashers import make_password
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.response import Response
+from rest_framework import status
 
 from ..models.user_details import UserDetails, User
 
@@ -12,7 +13,7 @@ class RegisterView(ObtainAuthToken):
             user, created_status = User.objects.get_or_create(username=details['email'])
             print(created_status)
             if not created_status:
-                return Response({'User Already Exists'})
+                return Response({'response': 'User Already Exists', 'status': status.HTTP_207_MULTI_STATUS})
             user.first_name = details['fname']
             user.last_name = details['lname']
             user.email = details['email']
@@ -29,11 +30,11 @@ class RegisterView(ObtainAuthToken):
             )
             if details['bike_model']:
                 userdetails.own_bike = 1
-                userdetails.save()
-            return Response({'User Created Successfully!'})
+            userdetails.save()
+            return Response({'response': 'User Created Successfully!', 'status': status.HTTP_200_OK})
 
         except Exception as error:
-            return Response({'Bad Request - Exception Occured!'})
+            return Response({'response': 'Bad Request', 'status': status.HTTP_400_BAD_REQUEST})
 
 
 
